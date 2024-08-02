8438563d35d8863d342b5e1f09e4bbdbac88ce50
NetflixClone <hosiduc2002.chem@gmail.com>
2024-08-02 16:39:24 +0700

    init: Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="font-semibold">Hello world</Button>
+    </>
+  );
+}
+
+export default App;
